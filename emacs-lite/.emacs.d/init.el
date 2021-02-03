ce07096790d3a0654aa822aecbc9e6dc25b03f2b
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2021-02-03 12:54:48 +0100

    Add emacs alternative configuration

---
+++ emacs-lite/.emacs.d/init.el
@@ -0,0 +1,7 @@
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(setq inhibit-splash-screen 1)
+
+(global-set-key (kbd "C-s") 'save-buffer)
+(global-set-key (kbd "C-w") 'save-buffers-kill-terminal)
