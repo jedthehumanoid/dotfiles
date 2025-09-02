a9f3bd14f1ad7504b2d89bb526df489242aeeb4c
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2025-09-02 12:34:51 +0200

    Delete emacs-lite directory

--- emacs-lite/.emacs.d/init.el
+++
@@ -1,7 +0,0 @@
-(menu-bar-mode -1)
-(toggle-scroll-bar -1)
-(tool-bar-mode -1)
-(setq inhibit-splash-screen 1)
-
-(global-set-key (kbd "C-s") 'save-buffer)
-(global-set-key (kbd "C-w") 'save-buffers-kill-terminal)
